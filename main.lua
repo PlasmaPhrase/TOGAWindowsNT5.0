3e66787687f1b11ad5e675df8f1278aaf354f767
Plasma <plasmaphrase@gmail.com>
2026-02-05 18:47:56 +0000

    Initial commit

---
+++ main.lua
@@ -0,0 +1,28 @@
+SMODS.Atlas({
+    key = "modicon", 
+    path = "ModIcon.png", 
+    px = 34,
+    py = 34,
+    atlas_table = "ASSET_ATLAS"
+})
+
+SMODS.Atlas({
+    key = "balatro", 
+    path = "balatro.png", 
+    px = 333,
+    py = 216,
+    prefix_config = { key = false },
+    atlas_table = "ASSET_ATLAS"
+})
+
+
+SMODS.Atlas({
+    key = "CustomJokers", 
+    path = "CustomJokers.png", 
+    px = 71,
+    py = 95, 
+    atlas_table = "ASSET_ATLAS"
+})
+
+assert(SMODS.load_file("src/jokers.lua"))()
+assert(SMODS.load_file("src/sounds.lua"))()
